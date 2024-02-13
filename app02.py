7fc1aedfc8db795ec841f5f01f1a7c70d1954ea9
Natthaphong66 <159878532+Natthaphong66@users.noreply.github.com>
2024-02-13 16:57:43 +0700

    Add files via upload

---
+++ app02.py
@@ -0,0 +1,9 @@
+#streamlit run streamlit/app02.py
+import streamlit as st 
+st.sidebar.text('Sidbar')
+st.sidebar.button('go>')
+st.sidebar.button('went>')
+st.sidebar.button('gone>')
+st.title('OOP in ML')
+st.image('balo.jpg')
+st.text_input('What is  your name')
